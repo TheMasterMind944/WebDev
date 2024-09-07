974b68e1d2d1c5c01691cf2d3671bd2766db149a
Horst Biesinger <Horst.Biesinger@online.de>
2024-09-07 12:25:30 +0200

    adding lessons

---
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,8 @@
+
+document.querySelector(".w.drum").addEventListener("click", () => {new Audio("./sounds/tom-1.mp3").play()});
+document.querySelector(".a.drum").addEventListener("click", () => {new Audio("./sounds/tom-2.mp3").play()});
+document.querySelector(".s.drum").addEventListener("click", () => {new Audio("./sounds/tom-3.mp3").play()});
+document.querySelector(".d.drum").addEventListener("click", () => {new Audio("./sounds/tom-4.mp3").play()});
+document.querySelector(".j.drum").addEventListener("click", () => {new Audio("./sounds/tom-1.mp3").play()});
+document.querySelector(".k.drum").addEventListener("click", () => {new Audio("./sounds/tom-1.mp3").play()});
+document.querySelector(".l.drum").addEventListener("click", () => {new Audio("./sounds/tom-1.mp3").play()});
